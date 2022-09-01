632a286186366ed3211c079497e4423572a5fd34
Sam Avery <sam.avery1@gmail.com>
2022-08-31 19:27:37 -0500

    commit tree 4249486b550686c74cb50a23337692b9ffe0ee08

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+const element = document.getElementById('main')
+element.remove();
+
+const newHeader = document.createElement("h1")
+// document.body.append(createElement)
+
+
+// newHeader.setAttribute("id", "victory")
+// newHeader.textContent = "is the champion"
+
+newHeader.id = "victory"
+newHeader.innerHTML = "is the champion"
